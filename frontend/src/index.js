f981bf8fc0bf5d225f8c6683a8b061bf103f9ef7
Adarsha Ghimire <adarshghimire2005@gmail.com>
2025-03-18 19:42:15 +0545

    Modified colors and code improvement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ const skillsPage = document.getElementById("skills-page");
 const projectsPage = document.getElementById("projects-page");
 
 document.querySelector("#home-button").addEventListener("click", () => {
-    document.body.style.overflow = "hidden";
     homePage.style.display = "flex"; 
 
     setTimeout(() => {
@@ -11,42 +10,34 @@ document.querySelector("#home-button").addEventListener("click", () => {
         projectsPage.style.transform = "translateX(100%)";  
         homePage.style.transform = "translateX(0)"; 
     }, 0); 
-
-    setTimeout(() => {
-        skillsPage.style.display = "none";
-        projectsPage.style.display = "none";
-        document.body.style.overflow = "auto";
-    }, 1100); 
 });
 
 document.querySelector("#skills-btn").addEventListener("click", function () {
     document.body.style.overflow = "hidden";
     skillsPage.style.display = "flex"; 
+    document.querySelector("#home-button").style.pointerEvents = "none";
 
     setTimeout(() => {
         homePage.style.transform = "translateX(100%)";
         skillsPage.style.transform = "translateX(0)";  
     }, 0);
 
- 
-    setTimeout(() => {
-        homePage.style.display = "none";
-        document.body.style.overflow = "auto"; 
+    setTimeout(() => { 
+        document.querySelector("#home-button").style.pointerEvents = "auto";
     }, 1100); 
 });
 
 document.querySelector("#projects-btn").addEventListener("click", function () {
     document.body.style.overflow = "hidden";
     projectsPage.style.display = "flex"; 
+    document.querySelector("#home-button").style.pointerEvents = "none";
 
     setTimeout(() => {
         homePage.style.transform = "translateX(-100%)";
         projectsPage.style.transform = "translateX(0)";  
     }, 0);
 
- 
     setTimeout(() => {
-        homePage.style.display = "none";
-        document.body.style.overflow = "auto"; 
+        document.querySelector("#home-button").style.pointerEvents = "auto";
     }, 1100); 
 });
