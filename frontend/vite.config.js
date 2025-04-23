737077ad8571e7ba6eb505c85089f1eac540ae04
Adarsha Ghimire <adarshghimire2005@gmail.com>
2025-04-24 00:50:02 +0545

    Moved all pages to root directory and modified vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,23 @@ const backendPORT = 5500;
 
 export default defineConfig({
     server: {
-        port: frontendPORT,
-        proxy: {
-          '/api': {
-            target: `http://localhost:${backendPORT}`, 
-            changeOrigin: true
-          }
+      port: frontendPORT,
+      proxy: {
+        '/api': {
+          target: `http://localhost:${backendPORT}`, 
+          changeOrigin: true
         }
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+          adminlogin: 'admin-login.html',
+          adminpage: 'admin-page.html',
+        },
       },
+    },
     plugins: [
         tailwindcss(),
     ],
