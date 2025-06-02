c896c7c0068785bffa4e73a79f41ade85187b3c4
Adarsha Ghimire <adarshghimire2005@gmail.com>
2025-06-02 10:49:00 +0545

    Content load message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 axios.defaults.withCredentials = true;
 
-const baseURL = import.meta.env.VITE_ENV === 'production' ? import.meta.env.VITE_URL : '';
+const baseURL =
+    import.meta.env.VITE_ENV === "production" ? import.meta.env.VITE_URL : "";
 
 const homePage = document.getElementById("home-page");
 const skillsPage = document.getElementById("skills-page");
@@ -10,83 +11,95 @@ const aboutPage = document.getElementById("about-page");
 const contactPage = document.getElementById("contact-page");
 
 document.querySelector("#home-button").addEventListener("click", () => {
-    homePage.style.display = "flex"; 
+    homePage.style.display = "flex";
     document.querySelector("#about-btn").style.pointerEvents = "auto";
     document.querySelector("#contact-btn").style.pointerEvents = "auto";
 
     setTimeout(() => {
-        skillsPage.style.transform = "translateX(-100%)";  
-        projectsPage.style.transform = "translateX(100%)";  
-        aboutPage.style.transform = "translateY(-100%)";  
-        contactPage.style.transform = "translateY(100%)";  
-        homePage.style.transform = "translateX(0)"; 
-    }, 0); 
+        skillsPage.style.transform = "translateX(-100%)";
+        projectsPage.style.transform = "translateX(100%)";
+        aboutPage.style.transform = "translateY(-100%)";
+        contactPage.style.transform = "translateY(100%)";
+        homePage.style.transform = "translateX(0)";
+    }, 0);
 });
 
 document.querySelector("#skills-btn").addEventListener("click", function () {
     document.body.style.overflow = "hidden";
-    skillsPage.style.display = "flex"; 
+    skillsPage.style.display = "flex";
     document.querySelector("#home-button").style.pointerEvents = "none";
     document.querySelector("#about-btn").style.pointerEvents = "none";
     document.querySelector("#contact-btn").style.pointerEvents = "none";
 
     setTimeout(() => {
         homePage.style.transform = "translateX(100%)";
-        skillsPage.style.transform = "translateX(0)";  
+        skillsPage.style.transform = "translateX(0)";
     }, 0);
 
-    setTimeout(() => { 
+    setTimeout(() => {
         document.querySelector("#home-button").style.pointerEvents = "auto";
-    }, 1100); 
+    }, 1100);
 });
 
 document.querySelector("#works-btn").addEventListener("click", function () {
     document.body.style.overflow = "hidden";
-    projectsPage.style.display = "flex"; 
+    projectsPage.style.display = "flex";
     document.querySelector("#home-button").style.pointerEvents = "none";
     document.querySelector("#about-btn").style.pointerEvents = "none";
     document.querySelector("#contact-btn").style.pointerEvents = "none";
 
     setTimeout(() => {
         homePage.style.transform = "translateX(-100%)";
-        projectsPage.style.transform = "translateX(0)";  
+        projectsPage.style.transform = "translateX(0)";
     }, 0);
 
     setTimeout(() => {
         document.querySelector("#home-button").style.pointerEvents = "auto";
-    }, 1100); 
+    }, 1100);
+
+    setTimeout(() => {
+        alert(
+            "Content of this page might require 15-20 seconds to load due to cold start of server."
+        );
+    }, 1000);
 });
 
 document.querySelector("#about-btn").addEventListener("click", function () {
     document.body.style.overflow = "hidden";
-    aboutPage.style.display = "flex"; 
+    aboutPage.style.display = "flex";
     document.querySelector("#home-button").style.pointerEvents = "none";
     document.querySelector("#contact-btn").style.pointerEvents = "none";
 
     setTimeout(() => {
         homePage.style.transform = "translateY(100%)";
-        aboutPage.style.transform = "translateY(0)";  
+        aboutPage.style.transform = "translateY(0)";
     }, 0);
 
     setTimeout(() => {
         document.querySelector("#home-button").style.pointerEvents = "auto";
-    }, 1100); 
+    }, 1100);
+
+    setTimeout(() => {
+        alert(
+            "Content of this page might require 15-20 seconds to load due to cold start of server."
+        );
+    }, 1000);
 });
 
 document.querySelector("#contact-btn").addEventListener("click", function () {
     document.body.style.overflow = "hidden";
-    contactPage.style.display = "flex"; 
+    contactPage.style.display = "flex";
     document.querySelector("#home-button").style.pointerEvents = "none";
     document.querySelector("#about-btn").style.pointerEvents = "none";
 
     setTimeout(() => {
         homePage.style.transform = "translateY(-100%)";
-        contactPage.style.transform = "translateY(0)";  
+        contactPage.style.transform = "translateY(0)";
     }, 0);
 
     setTimeout(() => {
         document.querySelector("#home-button").style.pointerEvents = "auto";
-    }, 1100); 
+    }, 1100);
 });
 
 //theme
@@ -100,13 +113,13 @@ themeBtn.addEventListener("click", () => {
     if (theme === "light") {
         localStorage.setItem("theme", "dark");
         document.documentElement.classList.add("dark");
-        const iconElement = document.querySelector('.theme-button i');
+        const iconElement = document.querySelector(".theme-button i");
         iconElement.classList.remove("fas", "fa-moon");
         iconElement.classList.add("fas", "fa-circle");
     } else {
         localStorage.setItem("theme", "light");
         document.documentElement.classList.remove("dark");
-        const iconElement = document.querySelector('.theme-button i');
+        const iconElement = document.querySelector(".theme-button i");
         iconElement.classList.remove("fas", "fa-circle");
         iconElement.classList.add("fas", "fa-moon");
     }
@@ -116,12 +129,11 @@ themeBtn.addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("theme") === "dark") {
         document.documentElement.classList.add("dark");
-        const iconElement = document.querySelector('.theme-button i');
+        const iconElement = document.querySelector(".theme-button i");
         iconElement.classList.add("fas", "fa-circle");
-    }
-    else {
+    } else {
         document.documentElement.classList.remove("dark");
-        const iconElement = document.querySelector('.theme-button i');
+        const iconElement = document.querySelector(".theme-button i");
         iconElement.classList.add("fas", "fa-moon");
     }
 });
@@ -201,7 +213,7 @@ function typeout() {
                 elem.innerHTML = elem.innerHTML.slice(0, -1);
             } else {
                 clearInterval(interval);
-                setTimeout(typein, 500);            
+                setTimeout(typein, 500);
             }
         }, 50);
     }
@@ -211,7 +223,6 @@ function typeout() {
 
 window.onload = typein;
 
-
 //contact
 const form = document.querySelector("#contact");
 form.addEventListener("submit", (event) => {
@@ -220,33 +231,32 @@ form.addEventListener("submit", (event) => {
     const data = {
         name: form.name.value,
         email: form.email.value,
-        message: form.message.value
+        message: form.message.value,
     };
 
-    axios.post(`${baseURL}/api/contact/send-message`, data)
-    .then(res => {
-        console.log(res.data);
-        form.reset();
-        document.querySelector("#error-message").innerHTML = "Message Sent Successfully";
-    })
-    .catch(err => {
-        console.error(err);
-    });
-}); 
+    axios
+        .post(`${baseURL}/api/contact/send-message`, data)
+        .then((res) => {
+            console.log(res.data);
+            form.reset();
+            document.querySelector("#error-message").innerHTML =
+                "Message Sent Successfully";
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
 
 //admin
 
 const adminBtn = document.querySelector("#admin");
 adminBtn.addEventListener("click", () => {
     if (localStorage.getItem("mode") == "visitor") {
-        axios.get(`${baseURL}/api/admin`)
-        .then((res) => {
+        axios.get(`${baseURL}/api/admin`).then((res) => {
             if (res.data.Authenticated)
-                window.location.href = 'admin-page.html';
-            else
-                window.location.href = 'admin-login.html';
-        })
-    }
-    else if (localStorage.getItem("mode") == "admin") 
-        window.location.href = '/';
-})
+                window.location.href = "admin-page.html";
+            else window.location.href = "admin-login.html";
+        });
+    } else if (localStorage.getItem("mode") == "admin")
+        window.location.href = "/";
+});
